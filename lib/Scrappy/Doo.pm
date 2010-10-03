569ea934dace34ad1c571dc449bbd641ddcc8f55
Al Newkirk <awncorp@cpan.org>
2010-10-03 23:21:21 +0000

    Import of AWNCORP/Scrappy-0.591 from CPAN.
    gitpan-cpan-distribution: Scrappy
    gitpan-cpan-version:      0.591
    gitpan-cpan-path:         AWNCORP/Scrappy-0.591.tar.gz
    gitpan-cpan-author:       AWNCORP
    gitpan-cpan-maturity:     released

---
+++ lib/Scrappy/Doo.pm
@@ -0,0 +1,31 @@
+# ABSTRACT: All Powerful Web Harvester, Spider, Scraper
+package Scrappy::Doo;
+BEGIN {
+  $Scrappy::Doo::VERSION = '0.591';
+}
+use base 'Scrappy';
+1;
+__END__
+=pod
+
+=head1 NAME
+
+Scrappy::Doo - All Powerful Web Harvester, Spider, Scraper
+
+=head1 VERSION
+
+version 0.591
+
+=head1 AUTHOR
+
+  Al Newkirk <awncorp@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2010 by awncorp.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
